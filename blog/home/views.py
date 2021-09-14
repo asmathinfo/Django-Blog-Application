f464f392bf7de86f393009ceaf189f8f56f3c83b
asmathinfo <asmath.info@gmail.com>
2021-09-15 00:08:49 +0530

    0 errors login page

--- blog/home/views.py
+++ blog/home/views.py
@@ -5,4 +5,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request , 'base.html' )
+    return render(request , 'home.html' )
+
+
+
+
+
+
+def login_view(request):
+    return render(request , 'login.html')
