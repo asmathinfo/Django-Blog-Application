8245216d42cd089420362cda02f623f68fb8903e
asmathinfo <asmath.info@gmail.com>
2021-09-14 23:20:16 +0530

    0 errors media path loop

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -13,8 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import stat
 from django.contrib import admin
+
+
 from django.urls import path , include
+
+from django.conf.urls.static import static
+
+from django.conf import settings
+
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('' , include('home.urls')),
@@ -24,3 +34,11 @@ urlpatterns = [
 
 
 ]
+
+""" this is media root path loop"""
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
--- blog/home/helpers.py
+++ blog/home/helpers.py
@@ -15,6 +15,10 @@ def generate_random_string(N):
 def generate_slug(text):
      new_slug = slugify(text)    
      from home.models import BlogModel
+
+
+
+     
      if BlogModel.objects.filter(slug = new_slug).first():
        generate_slug(text + generate_random_string(5))
      return new_slug
