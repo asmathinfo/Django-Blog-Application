95dd95b9762dafb48848629ffb28778980476b46
asmathinfo <asmath.info@gmail.com>
2021-09-15 00:17:13 +0530

    0 errors register page

--- blog/home/urls.py
+++ blog/home/urls.py
@@ -5,4 +5,9 @@ from .views import *
 urlpatterns = [
     
     path('' , home , name="home"),
+
+
+    path('login/' , login_view , name="login_view"),
+
+    path('register/' , register_view , name="register_view"),
 ]
--- blog/home/views.py
+++ blog/home/views.py
@@ -14,3 +14,8 @@ def home(request):
 
 def login_view(request):
     return render(request , 'login.html')
+
+
+
+def  register_view(request):
+    return render(request , 'register.html')
